792d63f4b904004063300a9f3698581af1f7202f
quocbinh93 <21012040@st.phenikaa-uni.edu.vn>
2023-04-08 20:46:53 +0700

    Initial commit

---
+++ src/main/java/applicationsfx/FileExplorer.java
@@ -0,0 +1,31 @@
+package applicationsfx;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
+public interface FileExplorer {
+    Image getIconImageFX(File f);
+    TreeItem<String>[] TreeCreate(File dir);
+    String calculateSize(File f);
+    String FindAbsolutePath(TreeItem<String> item, String s);
+    boolean IsDrive(File f);
+    int FilesHiddensCount(File dir);
+    void CreateTreeView(TreeView<String> treeview);
+    void CreateTableView(TableView<FileInfo> tableview, TableColumn<FileInfo, ImageView> image, TableColumn<FileInfo, String> date,
+                         TableColumn<FileInfo, String> name, TableColumn<FileInfo, String> size);
+    void CreateTableView();
+    void CreateTilesView();
+    void setLabelTxt();
+    void Initiate();
+    void setValues(TableView<FileInfo> tableview,TableColumn<FileInfo, ImageView> image,TableColumn<FileInfo, String> date,
+                   TableColumn<FileInfo, String> name,TableColumn<FileInfo, String> size);
+    void CreateTiles();
+    public int NumOfDirectChilds(File f);
+
+}
